a0ce9afcc27f013f976b8fd61a89e3d38b830069
cbowal <cbowal@gmail.com>
2014-02-22 20:58:23 -0500

    first

--- Form1.cs
+++ Form1.cs
@@ -79,14 +79,8 @@ namespace WindowsFormsKinectTest
         }
 
 
-        bool isReady = true;
         void SensorFrameReady(AllFramesReadyEventArgs e)
         {
-            if (isReady == false)
-            {
-                System.Console.WriteLine("SKIPPING");
-                return;
-            }
 
             // if the window is displayed, show the depth buffer image
             if (WindowState != FormWindowState.Minimized)
@@ -122,24 +116,17 @@ namespace WindowsFormsKinectTest
 
                 using (var frame = e.OpenColorImageFrame())
                 {
-                    Bitmap colorBitmap = ImageToBitmap(frame);
-                    if (colorBitmap == null)
+                    _bitmap = ImageToBitmap(frame);
+                    if (_bitmap == null)
                     {
-                        isReady = true;
                         return;
                     }
                     if (mask != null)
                     {
                         ApplyMask subtract = new ApplyMask(mask);
-                        colorBitmap = subtract.Apply(colorBitmap);
+                        _bitmap = subtract.Apply(_bitmap);
                     }
 
-
-                    // create filter
-                    Median med = new Median();
-                    //System.Console.WriteLine(colorBitmap.PixelFormat);
-                    // apply the filter
-                    med.ApplyInPlace(colorBitmap);
 
                     // create filter
                     ColorFiltering filter = new ColorFiltering();
@@ -148,24 +135,20 @@ namespace WindowsFormsKinectTest
                     filter.Green = new IntRange(50, 130);
                     filter.Blue = new IntRange(50, 130);
                     // apply the filter
-                    //System.Console.WriteLine(colorBitmap.PixelFormat);
-                    filter.ApplyInPlace(colorBitmap);
+                    filter.ApplyInPlace(_bitmap);
 
-                    // create filter
-                    HSLFiltering hsl = new HSLFiltering();
-                    // set color ranges to keep
-                    hsl.Hue = new IntRange(0, 360);
-                    hsl.Saturation = new Range(0f, 1);
-                    hsl.Luminance = new Range(0.5f, 1);
+                    // create and configure the filter
+                    FillHoles holes = new FillHoles();
+                    holes.MaxHoleHeight = 20;
+                    holes.MaxHoleWidth = 20;
+                    holes.CoupledSizeFiltering = false;
                     // apply the filter
-                    filter.ApplyInPlace(colorBitmap);
+                    Bitmap result = filter.Apply(_bitmap);
 
                     
                     Grayscale gray = new Grayscale(0.4,0.7,0.1);
-                    colorBitmap = gray.Apply(colorBitmap);
+                    _bitmap = gray.Apply(_bitmap);
 
-                    _bitmap = colorBitmap;
-                    isReady = true;
                     return;
                     
 
@@ -175,15 +158,15 @@ namespace WindowsFormsKinectTest
                     blob.MinHeight = 20;
                     blob.MinWidth = blob.MinHeight;
                     blob.MaxHeight = blob.MaxWidth = 100;
-                    blob.ApplyInPlace(colorBitmap);
+                    blob.ApplyInPlace(_bitmap);
 
                     // locate objects using blob counter
                     BlobCounter blobCounter = new BlobCounter();
-                    blobCounter.ProcessImage(colorBitmap);
+                    blobCounter.ProcessImage(_bitmap);
                     Blob[] blobs = blobCounter.GetObjectsInformation();
                     // create Graphics object to draw on the image and a pen
-                    colorBitmap = AForge.Imaging.Image.Clone(colorBitmap, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-                    Graphics g = Graphics.FromImage(colorBitmap);
+                    _bitmap = AForge.Imaging.Image.Clone(_bitmap, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                    Graphics g = Graphics.FromImage(_bitmap);
                     Pen redPen = new Pen(Color.Red, 2);
                     Pen greenPen = new Pen(Color.Green, 2);
                     Pen purplePen = new Pen(Color.Purple, 5);
@@ -229,10 +212,8 @@ namespace WindowsFormsKinectTest
                     redPen.Dispose();
                     g.Dispose();
 
-                    _bitmap = colorBitmap;
                 }
 
-                isReady = true;
             }
         }
 
